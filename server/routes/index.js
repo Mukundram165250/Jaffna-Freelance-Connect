f9df84c78d16e0afedb811d01cf7571283c43b84
Mukundram <mukundram165250@gmail.com>
2026-09-25 21:03:01 +0530

    feat(admin): mount protected admin routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,7 @@ const authRouter = require("./auth");
 const profileRouter = require("./profile");
 const jobsRouter = require("./jobs");
 const applicationsRouter = require("./applications");
+const adminRouter = require("./admin");
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.use("/auth", authRouter);
 router.use("/profile", profileRouter);
 router.use("/jobs", jobsRouter);
 router.use("/applications", applicationsRouter);
+router.use("/admin", adminRouter);
 
 module.exports = router;
